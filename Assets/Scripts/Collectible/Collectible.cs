14120f4116341152be1c9440348023d9fe57376c
KMOzk <210372@student.glu.nl>
2023-04-25 15:43:04 +0200

    colitcble push

--- Assets/Scripts/Collectible/Collectible.cs
+++ Assets/Scripts/Collectible/Collectible.cs
@@ -2,28 +2,22 @@ using TMPro;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Collectible : MonoBehaviour
 {
-    private int score;
     [SerializeField] private int itemValue;
-
-    [SerializeField] private TextMeshProUGUI collectibleText;
-
-    void Start() { UpdateText(); }
+    [SerializeField] private TMP_Text valueText;
 
     private void OnTriggerEnter(Collider other)
     {
         if (Timer.instance.timeLeft > 0 && other.gameObject.CompareTag("Player"))
         {
-            score += itemValue;
-            UpdateText();
+            valueText.text    = itemValue.ToString();
+            valueText.enabled = true;
+            CollectibleHandle.Instance.UpdateText(itemValue);
+
             Destroy(this.gameObject);
         }
-    }
-
-    private void UpdateText()
-    { 
-        collectibleText.text = score.ToString();     
     }
 }
--- Assets/Scripts/Collectible/CollectibleHandle.cs
+++ Assets/Scripts/Collectible/CollectibleHandle.cs
@@ -1,16 +1,21 @@
+using TMPro;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public class CollectibleHandle : MonoBehaviour
-{
-    void Start()
-    {
-        
-    }
+{ 
+    public static CollectibleHandle Instance;
 
-    void Update()
+    private int score;
+
+    [SerializeField] private TextMeshProUGUI collectibleText;
+
+    void Start() { UpdateText(0); }
+
+    public void UpdateText(int value)
     {
-        
+        score += value;
+        collectibleText.text = score.ToString();
     }
 }
