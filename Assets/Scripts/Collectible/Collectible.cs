a4d04e21cc410878437d0842aa75ad1d91402004
KMOzk <210372@student.glu.nl>
2023-04-26 01:33:20 +0200

    changes for the collectibles

--- Assets/Scripts/Collectible/Collectible.cs
+++ Assets/Scripts/Collectible/Collectible.cs
@@ -6,24 +6,35 @@ using System.Collections.Generic;
 public class Collectible : MonoBehaviour
 {
     private int score;
+    private Rigidbody rigidbodyItem;
+
     [SerializeField] private int itemValue;
+    [SerializeField] private float deathTime;
 
     [SerializeField] private TextMeshProUGUI collectibleText;
 
-    void Start() { UpdateText(); }
+    void Start() { UpdateText(); rigidbodyItem = GetComponent<Rigidbody>(); }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //CarController player = other.GetComponent<CarController>();
+            Vector3 Direction = (other.transform.position - transform.position).normalized;
+      //      rigidbodyItem.AddForce(Direction);
+            transform.Translate(Direction);
+        }
+
         if (Timer.instance.timeLeft > 0 && other.gameObject.CompareTag("Player"))
         {
             score += itemValue;
             UpdateText();
-            Destroy(this.gameObject);
+            Destroy(this.gameObject, deathTime);
         }
     }
 
     private void UpdateText()
-    { 
-        collectibleText.text = score.ToString();     
+    {
+        collectibleText.text = score.ToString();
     }
 }
--- Assets/Scripts/Collectible/CollectibleHandle.cs
+++ Assets/Scripts/Collectible/CollectibleHandle.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class CollectibleHandle : MonoBehaviour
 {
@@ -13,4 +14,5 @@ public class CollectibleHandle : MonoBehaviour
     {
         
     }
+
 }
